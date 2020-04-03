7d05ecf64ab72303273b5f3e869a42b213dc6a2f
TheFlyer1983 <theflyer1983@gmail.com>
2020-04-03 20:50:02 +0100

    Assignment 5

--- Section2/Assignment4/app.js
+++ Section2/Assignment4/app.js
@@ -1,11 +1,60 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    effect: false,
+    style: '',
+    style2: '',
+    style3: '',
+    attached: false,
+    color: '',
+    width: 0,
+  },
+  computed: {
+    myStyle: function(){
+      return {
+        backgroundColor: this.color,
+        color: 'white'
+      };
+    },
+    progress: function(){
+      return{
+        backgroundColor: 'blue',
+        width: this.width + '%'
+      }
+    }
+  },
+  watch:{
+    effect: function(){
+      var vm = this;
+      setTimeout(function(){
+        if (vm.effect === 'highlight'){
+          vm.effect = 'shrink';
+        } else {
+          vm.effect = 'highlight'
+        }
+      }, 5000);
+    },
+    attached: function(){
+      var vm = this;
+      if (vm.attached === 'true'){
+        vm.style3 = 'text-color';
+      } else {
+        vm.style3 = '';
+      };
+    },
+    width: function(){
+      var vm = this;
+      setTimeout(function(){
+        vm.width += 10
+      },2000);
+    }
   },
   methods: {
     startEffect: function() {
-    
+      return this.effect = 'highlight';
+    },
+    startProgress: function() {
+      return this.width += 10;
     }
   }
 });
