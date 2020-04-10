c4cb7310cba427a392916c2a8727dd6facaeabfd
TheFlyer1983 <theflyer1983@gmail.com>
2020-04-10 19:16:32 +0100

    Assignment 7 - Components

---
+++ Section07/Assignment06/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+      <heading></heading>  
+      <hr>
+      <servers></servers>
+      <hr>
+      <my-footer></my-footer>
+    </div>
+</template>
+
+<script>
+  import Header from "./Header.vue";
+  import Footer from "./Footer.vue";
+  import Servers from "./Servers.vue";
+  export default {
+    components: {
+      'heading': Header,
+      'my-footer': Footer,
+      'servers': Servers
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ Section07/Assignment06/src/Servers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <server-list></server-list>
+    <server-details></server-details>
+  </div>
+</template>
+
+<script>
+  import ServerDetails from './ServerDetails.vue';
+  import ServerList from './ServerList.vue';
+  export default {
+    components: {
+      'server-details': ServerDetails,
+      'server-list': ServerList
+    }
+  }
+</script>
