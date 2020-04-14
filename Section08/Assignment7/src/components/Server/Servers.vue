35526cdc47581c7073f44023fb0bff4e9ab5e1f7
TheFlyer1983 <theflyer1983@gmail.com>
2020-04-14 09:03:46 +0100

    Assignment 7 files

--- Section08/Assignment7/src/components/Server/Servers.vue
+++ Section08/Assignment7/src/components/Server/Servers.vue
@@ -1,16 +1,38 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
+      <ul class="list-group">
+        <app-server
+          v-for="server in servers"
+          :server="server"  
+        ></app-server>
+            <!-- <li
                     class="list-group-item"
                     v-for="index in 5">
                 Server #{{ index }}
-            </li>
-        </ul>
+            </li> -->
+      </ul>
     </div>
 </template>
 
 <script>
+  import AppServer from './AppServer.vue';
+
+  export default {
+    data() {
+      return {
+        servers: [
+          { id: 1, status: 'Normal' },
+          { id: 2, status: 'Critical' },
+          { id: 3, status: 'Unknown' },
+          { id: 4, status: 'Normal' },
+          { id: 5, status: 'Critical' }
+        ]
+      }
+    },
+    components: {
+      'app-server': AppServer
+    }
+  }
 </script>
 
 <style>
