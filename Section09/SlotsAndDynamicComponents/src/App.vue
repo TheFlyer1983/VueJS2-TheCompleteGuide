5c179c4a0fd41ebdadda24db2c89dcc941c100ab
TheFlyer1983 <theflyer1983@gmail.com>
2020-04-15 20:31:04 +0100

    Slots & Dynamic Components, Assignment 8 files

---
+++ Section09/SlotsAndDynamicComponents/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <!-- <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A Wonderful Quote</p>
+        </app-quote>   -->
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>Default Content</p>
+          </component>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+  import Author from './components/Author.vue';
+  import New from './components/New.vue';
+
+  export default {
+    data(){
+      return {
+        quoteTitle: 'The Quote',
+        selectedComponent: 'appQuote'
+      }
+    },
+    components: {
+      appQuote: Quote,
+      appAuthor: Author,
+      appNew: New
+    }
+  }
+</script>
+
+<style scoped>
+  h2 {
+    color: blue;
+  }
+</style>
