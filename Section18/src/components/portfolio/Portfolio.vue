a9aed8c2436f7c993c158c4523fbddbd791fd3f4
TheFlyer1983 <theflyer1983@gmail.com>
2020-04-29 16:29:54 +0100

    buy and sell stocks

--- Section18/src/components/portfolio/Portfolio.vue
+++ Section18/src/components/portfolio/Portfolio.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>The Portfolio Component</h1>
+  <div>
+    <portfolio-stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+  </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import Stock from "./Stock.vue";
+
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: "stocksPortfolio"
+    })
+  },
+  components: {
+    portfolioStock: Stock
+  }
+};
+</script>
